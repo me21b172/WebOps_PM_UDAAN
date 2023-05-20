38e7582103bed59ad843503d09ccc65490d8e541
Anuj Jagannath Said <me21b172@smail.iitm.ac.in>
2023-05-20 17:35:57 +0530

    second_commit

--- fetch.js
+++ fetch.js
@@ -7,5 +7,5 @@ async function Data() {
     audio.pause();
   }
 document.getElementById("strtbtn").addEventListener("click",function(){
-     setInterval(Data, 5000);
+     setInterval(Data, 500);
 });
